d3be9173df13ddbc24cef15f18569e09c0747cec
Colin Huckestein <cphuckestein@gmail.com>
2021-04-12 09:57:55 -0500

    merge search branch

--- src/client/components/SearchBar.tsx
+++ src/client/components/SearchBar.tsx
@@ -7,19 +7,9 @@ import {
   useHistory,
 } from "react-router-dom";
 
-// import './SearchBarStyles.css'
-
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const history = useHistory();
-
-  // const handleHistory = () => {
-  //   history.push('/search')
-  // }
-
-  // const handleSumbit = e => {
-  //   e.preventDefault();
-  // }
 
   return (
     <div
@@ -44,86 +34,3 @@ const SearchBar = () => {
 
 export default SearchBar;
 
-// const SearchBar = () => {
-//   const [searchTerm, setSearchTerm] = useState("");
-//   const history = useHistory();
-//   const [autocomplete, setAutocomplete] = useState([]);
-//   const handleHistory = () => {
-//     setSearchTerm("");
-//     history.push("/");
-//   };
-
-//   const getMovies = async () => {
-//     const fetchData = await fetch(
-//       `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${searchTerm}&page=1&include_adult=false`
-//     ).then((res) => res.json().then((res) => setAutocomplete(res)));
-//     console.log(fetchData);
-//   };y
-
-//   const key = `14257b7461dc5f7e2f6cf229f055bf83`;
-//   return (
-//     <div
-//       id="search-bar"
-//       className="font-weight-light mx-4 border-bottom border-primary font-weight-bold"
-//     >
-//       <ReactLiveSearch
-//         className="form-control form-control-md"
-//         value={searchTerm}
-//         // onChange={(e) => setSearchTerm(e.target.value)}
-//         placeholder="Explore films"
-//         data={["one", "two", ]}
-//       />
-
-//       <Link to={`/${searchTerm}`}>
-//         <button
-//           type="submit"
-//           className="btn btn-primary"
-//           onChange={handleHistory}
-//         >
-//           Search
-//         </button>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default SearchBar;
-
-{
-  /* <input
-        className="form-control form-control-md"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Explore films"
-      /> */
-}
-
-//history.push
-// const Search = (props) => {
-//     const [searchValue, setSearchValue] = useState("");
-
-//     const handleSearchInputChanges = (e) => {
-//       setSearchValue(e.target.value);
-//     }
-
-//     const resetInputField = () => {
-//       setSearchValue("")
-//     }
-
-//     const callSearchFunction = (e) => {
-//       e.preventDefault();
-//       props.search(searchValue);
-//       resetInputField();
-//     }
-
-//     return (
-//         <form className="search">
-//           <input
-//             value={searchValue}
-//             onChange={handleSearchInputChanges}
-//             type="text"
-//           />
-//           <input onClick={callSearchFunction} type="submit" value="SEARCH" />
-//         </form>
-//       );
-//   }
--- src/client/components/SearchResults.tsx
+++ src/client/components/SearchResults.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import SearchBar from "./SearchBar";
 import { nanoid } from 'nanoid';
 
 const SearchResults = () => {
   const [results, setResults] = useState([]);
   const { searchTerm } = useParams<ParamsProps>();
-  // const key = process.env.REACT_APP_Movie_DB_API_Key;
   const key = `14257b7461dc5f7e2f6cf229f055bf83`;
 
   const getMovies = async () => {
@@ -41,14 +39,6 @@ const SearchResults = () => {
                 <div className="card-body">
                 <Link className="h6 text-truncate text-dark pt-2" 
                 to={`/film/${result.id}`}>{result.title}</Link>
-                  {/* <Link className="h6 text-truncate" to={`/film/${result.id}`}>
-                    <h2
-                      className="card-title "
-                      style={{ maxWidth: "6rem;" }}
-                    >
-                      {result.title}
-                    </h2>{" "}
-                  </Link> */}
                   <img
                     src={`https://image.tmdb.org/t/p/w92${result.poster_path}`}
                     alt="cap"
@@ -62,46 +52,13 @@ const SearchResults = () => {
     </>
   );
 };
-{
-  /* <div
-            className="col-md text-grey bg-light mb-3 d-flex flex-row"
-            style={{ maxWidth: "20rem" }}
-            >
-            <h1 id="text-center search-result-text">Search Results: </h1>
-            </div>
-            <div
-            className="col-md-12 d-flex flex-wrap justify-content-center"
-            > 
-
-          one container div with flexbox, inside the image inside 
-          <div style={{ maxWidth: "18rem" }}>
-          <img className="img-top" src={`https://image.tmdb.org/t/p/w92/${result.poster_path}`} alt="image cap" />
-             <div className="body">
-              {/* <h5 className="card-title">Card title</h5> */
-}
-{
-  /* <Link className="h2" key={index} to={`/film/${result.id}`}>
-                 {result.title}
-               </Link> */
-}
-{
-  /* <p className="card-text">
-                 {/* {result.overview} </p> */
-}
-
-/* <>
-      <h1 id="search-result-text">Search Results: </h1>
-      {results.map(result => <Link className="h1" to={`/film/${result.id}`}>{result.title}</Link>)}
-    </> */
-
-//history.push
 
 export default SearchResults;
 
 interface ParamsProps {
   searchTerm: string;
-}
+};
 
 interface SearchResults {
   searchTerm: string;
-}
+};
